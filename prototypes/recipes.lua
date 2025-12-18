551e858181c05af718045943c522239137c274bb
SerotoninTheft <87555495+SerotoninTheft@users.noreply.github.com>
2025-12-17 17:47:49 -0800

    yeah

---
+++ prototypes/recipes.lua
@@ -0,0 +1,16 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "belt-to-iron",
+    category = "crafting",
+    enabled = true,
+    ingredients = {
+      {"transport-belt", 4}
+    },
+    results = {
+      {"iron-plate", 7}
+    },
+    energy_required = 0.5,
+    order = "z[belt-to-iron]"
+  }
+})
